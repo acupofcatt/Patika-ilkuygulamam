65f33c606a0b8a1af37147ab345a1cac43a08f4f
acupofcatt <115213915+acupofcatt@users.noreply.github.com>
2024-10-01 22:30:35 +0300

    ilk uygulamam

---
+++ Patika+Week1/Patika+Week1/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\nElifgül Özgürbüz\nPatika sayesinde disiplin kazanmak ve iyi bir yazılımcı olma yolunda sağlam temeller edinmek için buradayım.");
